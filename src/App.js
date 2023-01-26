31521245793635ed88d1696d78d433ed1eb320cb
Jaival Upadhyay <57015292+jaivalupadhyay@users.noreply.github.com>
2023-01-26 16:01:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { Amplify } from "aws-amplify";
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
-import { SignInHeader } from "./SignInHeader";
-import { SignInFooter } from "./SignInFooter";
-import "./styles.css";
-
-import awsExports from "./aws-exports";
+import awsExports from './aws-exports';
+import React from 'react';
 Amplify.configure(awsExports);
 
-export function App({ signOut, user }) {
+
+function App({ signOut, user }) {
   return (
-    <main>
+    <div>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
-    </main>
+      </div>
   );
 }
 
-export default withAuthenticator(App, {
-  components: {
-    Header,
-    SignIn: {
-      Header: SignInHeader,
-      Footer: SignInFooter
-    },
-    Footer
-  }
-});
+export default withAuthenticator(App);
