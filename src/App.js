3492fc36134c2ce8b79fc376d5de6af221ad9b1b
Jaival Upadhyay <57015292+jaivalupadhyay@users.noreply.github.com>
2023-02-12 03:05:42 +0530

    Updated Files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Footer } from "./Footer";
 import { SignInHeader } from "./SignInHeader";
 import { SignInFooter } from "./SignInFooter";
 import "./styles.css";
+import {
+  EditProfile 
+ } from './ui-components';
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -13,6 +16,7 @@ Amplify.configure(awsExports);
 export function App({ signOut, user }) {
   return (
     <main>
+      <EditProfile />
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </main>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={studioTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   rootElement
 );
